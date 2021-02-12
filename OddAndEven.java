616b19aa0efa7f1bb5784adfe2ef32565250297e
good-intentions <72018474+good-intentions@users.noreply.github.com>
2021-02-12 11:05:51 +0200

    Add files via upload

--- OddAndEven.java
+++ OddAndEven.java
@@ -4,18 +4,25 @@ public class OddAndEven {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		odd(5);
-		even(5);
+		if(odd(5)) { 
+			System.out.print("Raboti");
+		}
+		if(even(4)){
+			System.out.print("Funkcionira");
+		}
 
 	}
 	public static boolean even(int x) {
+		
+		if(x==1) {
+			return false;
+		}
 		if(x==0) {
 			return true;
 		}
-		else if (x>1) {
-			even(x-2);
-		}
-		return false;
+		//else if (x>1) {
+		return even(x-2);
+		//}
 		
 	}
     public static boolean odd(int x) {
@@ -24,11 +31,7 @@ public class OddAndEven {
 		}else if(x==0) {
 			return false;
 		}
-		
-		else  if (x>1) {
-			odd(x-2);
-		}
-		return false;
+		return	odd(x-2);
 		
 	}
 
